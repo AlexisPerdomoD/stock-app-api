f8dc89d405abe1ece652aff0be30a5cef146b69a
alexisperdomod <dropdibemol@gmail.com>
2025-04-10 22:51:16 -0500

    feat: implement user basic use cases

--- internal/application/usecase/user/create.go
+++ internal/application/usecase/user/create.go
@@ -1,2 +1,38 @@
 package usecase
 
+import (
+	"github.com/alexisPerdomoD/stock-app-api/internal/domain/entity"
+	r "github.com/alexisPerdomoD/stock-app-api/internal/domain/repository"
+	s "github.com/alexisPerdomoD/stock-app-api/internal/shared"
+	ss "github.com/alexisPerdomoD/stock-app-api/internal/shared/service"
+)
+
+type createUserUseCase struct {
+	userRepository r.UserRepository
+}
+
+func (uc createUserUseCase) Execute(args *entity.User) error {
+
+	hashed, err := ss.HashPassword(args.Password)
+
+	if err != nil {
+		return s.InternalServerError("Error hashing password")
+	}
+
+	args.Password = hashed
+
+	if err := uc.userRepository.Create(args); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewCreateUserUseCase(ur r.UserRepository) *createUserUseCase {
+
+	if ur == nil {
+		panic("user repository is nil, stopping :b")
+	}
+
+	return &createUserUseCase{userRepository: ur}
+}
--- internal/application/usecase/user/login.go
+++ internal/application/usecase/user/login.go
@@ -1,2 +1,39 @@
 package usecase
 
+import (
+	r "github.com/alexisPerdomoD/stock-app-api/internal/domain/repository"
+	ss "github.com/alexisPerdomoD/stock-app-api/internal/shared/service"
+)
+
+type loginUseCase struct {
+	userRepository r.UserRepository
+}
+
+func (uc *loginUseCase) Execute(username, password string) (session string, err error) {
+	user, err := uc.userRepository.GetByUsername(username)
+
+	if err != nil {
+		return "", err
+	}
+
+	if err = ss.VerifyPassword(password, user.Password); err != nil {
+		return "", err
+	}
+
+	session, err = ss.GenerateSessionToken(user)
+
+	if err != nil {
+		return "", err
+	}
+
+	return session, nil
+}
+
+func NewLoginUseCase(ur r.UserRepository) *loginUseCase {
+
+	if ur == nil {
+		panic("user repository is nil, stopping :b")
+	}
+
+	return &loginUseCase{userRepository: ur}
+}
