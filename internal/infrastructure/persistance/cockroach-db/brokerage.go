7a127a39a8cf738e9cd27b8150e88cb078b89474
alexisperdomod <dropdibemol@gmail.com>
2025-04-13 16:54:03 -0500

    feat: implement brokerage repository with gorm

--- internal/infrastructure/persistance/cockroach-db/brokerage.go
+++ internal/infrastructure/persistance/cockroach-db/brokerage.go
@@ -2,6 +2,7 @@ package cockroachdb
 
 import (
 	"context"
+	"errors"
 	"log"
 
 	"github.com/alexisPerdomoD/stock-app-api/internal/domain"
@@ -13,21 +14,50 @@ type brokerageRepository struct {
 }
 
 func (r *brokerageRepository) Get(ctx context.Context, id uint) (*domain.Brokerage, error) {
+	record := &brokerageRecord{}
 
-	panic("Implement me")
+	if err := r.db.WithContext(ctx).First(record, id).Error; err != nil {
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+
+		return nil, err
+
+	}
+
+	return mapBrokerageToDomain(record, nil), nil
 
 }
 
 func (r *brokerageRepository) GetByName(ctx context.Context, name string) (*domain.Brokerage, error) {
 
-	panic("Implement me")
+	record := &brokerageRecord{}
+
+	if err := r.db.WithContext(ctx).First(record, "'name' = ?", name).Error; err != nil {
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+
+		return nil, err
+
+	}
+
+	return mapBrokerageToDomain(record, nil), nil
 
 }
 
-func (r *brokerageRepository) Create(ctx context.Context, id uint) error {
+func (r *brokerageRepository) Create(ctx context.Context, br *domain.Brokerage) error {
 
-	panic("Implement me")
+	record := mapBrokerageInsert(br)
 
+	if err := r.db.WithContext(ctx).Create(record).Error;err != nil{
+		return err
+	}
+
+	_ = mapBrokerageToDomain(record, br)
+	return nil
 }
 
 func NewBrokerageRepository(db *gorm.DB) *brokerageRepository {
