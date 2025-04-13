1b45e32b7fe6d384377a44a5c1f4bb2bceea9c4e
alexisperdomod <dropdibemol@gmail.com>
2025-04-13 16:51:56 -0500

    update: refine pagination pkg utilities

--- internal/pkg/pagination.go
+++ internal/pkg/pagination.go
@@ -7,22 +7,38 @@ const (
 	Desc SortOrder = "desc"
 )
 
+type FilterOperator string
+
+const (
+	Equals      FilterOperator = "equals"
+	GreaterThan FilterOperator = "greater_than"
+	LessThan    FilterOperator = "less_than"
+	NotEquals   FilterOperator = "not_equals"
+	IsNull      FilterOperator = "is_null"
+	IsNotNull   FilterOperator = "is_not_null"
+)
+
 type FilterByItem struct {
-	Field string      `json:"field"`
-	Value interface{} `json:"value"`
+	Field    string         `json:"field"`
+	Operator FilterOperator `json:"operator"`
+	Value    interface{}    `json:"value"`
+}
+
+type PaginationPage struct {
+	Page int `json:"page"`
+	Size int `json:"size"`
 }
 
 type PaginationFilter struct {
 	SortOrder SortOrder      `json:"sort_order"`
 	SortBy    string         `json:"sort_by"`
 	FilterBy  []FilterByItem `json:"filter_by"`
-	Page      int            `json:"page"`
-	Size      int            `json:"size"`
+	PaginationPage
 }
 
 type PaginationReponse[T interface{}] struct {
-	Items    []T `json:"items"`
-	Page     int `json:"page"`
-	PageSize int `json:"page_size"`
-	Total    int `json:"total"`
+	Items     []T `json:"items"`
+	Page      int `json:"page"`
+	PageSize  int `json:"page_size"`
+	TotalSize int `json:"total_size"`
 }
