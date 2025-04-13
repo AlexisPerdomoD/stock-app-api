224154e2cab7a4fc448a347f36eefac94f8780d4
alexisperdomod <dropdibemol@gmail.com>
2025-04-13 01:32:44 -0500

    feat: define record model and mappers to domain for cockroachdb repository implementations

---
+++ internal/infrastructure/persistance/cockroach-db/mappers.go
@@ -0,0 +1,152 @@
+package cockroachdb
+
+import "github.com/alexisPerdomoD/stock-app-api/internal/domain"
+
+/* All mappers insertions assume that the args are not nil */
+
+func mapMarketInsert(args *domain.Market) *marketRecord {
+	return &marketRecord{Name: args.Name}
+}
+
+/* mapCompanyInsert assumes that the args are not nil */
+func mapCompanyInsert(args *domain.Company) *companyRecord {
+
+	return &companyRecord{
+		MarketID: args.MarketID,
+		ISIN:     args.ISIN,
+		Name:     args.Name,
+	}
+}
+
+/* mapBrokerageInsert assumes that the args are not nil */
+func mapBrokerageInsert(args *domain.Brokerage) *brokerageRecord {
+	return &brokerageRecord{Name: args.Name}
+}
+
+/* mapStockInsert assumes that the args are not nil */
+func mapStockInsert(args *domain.Stock) *stockRecord {
+
+	return &stockRecord{
+		Name:      args.Name,
+		CompanyID: args.CompanyID,
+		Ticker:    args.Ticker,
+		Price:     args.Price,
+		Tendency:  args.Tendency,
+	}
+}
+
+/* mapRecommendationInsert assumes that the args are not nil */
+func mapRecommendationInsert(args *domain.Recommendation) *recommendationRecord {
+
+	return &recommendationRecord{
+		StockID:     args.StockID,
+		BrokerageID: args.BrokerageID,
+		RatingFrom:  args.RatingFrom,
+		RatingTo:    args.RatingTo,
+		TargetFrom:  args.TargetFrom,
+		TargetTo:    args.TargetFrom,
+	}
+}
+
+/* mapUserInsert assumes that the args are not nil */
+func mapUserInsert(args *domain.User) *userRecord {
+
+	return &userRecord{
+		UserName: args.UserName,
+		Password: args.Password,
+		Active:   args.Active,
+	}
+}
+
+/* mapMarketToDomain assumes that the record is not nil */
+func mapMarketToDomain(record *marketRecord, target *domain.Market) *domain.Market {
+
+	if target == nil {
+		return &domain.Market{
+			Name:      record.Name,
+			ID:        record.ID,
+			CreatedAt: record.CreatedAt,
+		}
+	}
+
+	target.ID = record.ID
+	target.Name = record.Name
+
+	return target
+}
+
+/* mapCompanyToDomain assumes that the record is not nil */
+func mapCompanyToDomain(record *companyRecord, target *domain.Company) *domain.Company {
+
+	if target == nil {
+		return &domain.Company{
+			ID:        record.ID,
+			MarketID:  record.MarketID,
+			Name:      record.Name,
+			ISIN:      record.ISIN,
+			CreatedAt: record.CreatedAt,
+		}
+	}
+	return nil
+}
+
+/* mapBrokerageToDomain assumes that the record is not nil */
+func mapBrokerageToDomain(record *brokerageRecord, target *domain.Brokerage) *domain.Brokerage {
+
+	if target == nil {
+		return &domain.Brokerage{
+			ID:        record.ID,
+			Name:      record.Name,
+			CreatedAt: record.CreatedAt,
+		}
+
+	}
+	return nil
+}
+
+/* mapStockToDomain assumes that the record is not nil */
+func mapStockToDomain(record *stockRecord, target *domain.Stock) *domain.Stock {
+
+	if target == nil {
+		return &domain.Stock{
+			ID:        record.ID,
+			CompanyID: record.CompanyID,
+			Ticker:    record.Ticker,
+			Name:      record.Name,
+			Tendency:  record.Tendency,
+			Price:     record.Price,
+			CreatedAt: record.CreatedAt,
+			UpdatedAt: record.UpdatedAt,
+		}
+
+	}
+	target.ID = record.ID
+	target.CompanyID = record.CompanyID
+	target.Ticker = record.Ticker
+	target.Name = record.Name
+	target.Tendency = record.Tendency
+	target.Price = record.Price
+	target.CreatedAt = record.CreatedAt
+	target.UpdatedAt = record.UpdatedAt
+
+	return target
+}
+
+/* mapUserToDomain assumes that the record is not nil */
+func mapUserToDomain(record *userRecord, target *domain.User) *domain.User {
+
+	if target != nil {
+		target.UserName = record.UserName
+		target.Password = record.Password
+		target.Active = record.Active
+		return target
+	}
+
+	return &domain.User{
+		ID:       record.ID,
+		UserName: record.UserName,
+		Password: record.Password,
+		Active:   record.Active,
+	}
+
+}
--- internal/infrastructure/persistance/cockroach-db/tables.go
+++ internal/infrastructure/persistance/cockroach-db/tables.go
@@ -5,20 +5,52 @@ import (
 	"gorm.io/gorm"
 )
 
-type userTable struct {
+type marketRecord struct {
+	gorm.Model
+	Name string `gorm:"unique"`
+}
+
+type companyRecord struct {
+	gorm.Model
+	MarketID uint
+	Name     string
+	ISIN     *string `gorm:"unique"`
+}
+
+type brokerageRecord struct {
+	gorm.Model
+	Name string `gorm:"unique"`
+}
+
+type stockRecord struct {
+	gorm.Model
+	Name      *string
+	CompanyID uint   `gorm:"uniqueIndex:idx_ticker_company"`
+	Ticker    string `gorm:"uniqueIndex:idx_ticker_company"`
+	Price     float64
+	Tendency  domain.Tendency
+}
+
+type recommendationRecord struct {
+	gorm.Model
+	StockID     uint
+	BrokerageID uint
+	RatingTo    domain.Action
+	RatingFrom  domain.Action
+	TargetTo    float64
+	TargetFrom  float64
+}
+
+type userRecord struct {
 	gorm.Model
 	UserName string `gorm:"unique"`
 	Password string
 	Active   bool
 }
 
-func (u *userTable) ToDomain() *domain.User {
-	
-	return &domain.User{
-		ID:       u.ID,
-		UserName: u.UserName,
-		Password: u.Password,
-		Active:   u.Active,
-	}
-
+type userStockRecord struct {
+	gorm.Model
+	UserID  uint `gorm:"uniqueIndex:idx_user_stock"`
+	StockID uint `gorm:"uniqueIndex:idx_user_stock"`
+	Count   uint `gorm:"default:1"`
 }
