3e8face751be9783528f16fcdddda2585e2c1181
alexisperdomod <dropdibemol@gmail.com>
2025-04-13 01:35:00 -0500

    doc: update todos for main.go

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+/* All rights and lefts reserved */
+
 func main() {
+
+	/*
+	1) Instance db
+	2) Inject db on repositories implementations
+	3) Inject repositories and services on usecases
+	4) Inject usecases on controllers
+	5) Start server
+	6) Map controllers routes
+	7) Pray Golang and start
+	*/
+
 	println("Hello, stock-app!")
 }
