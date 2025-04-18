fc42eafd17c86fb54a871f664084384c0b0e3322
alexisperdomod <dropdibemol@gmail.com>
2025-04-17 21:08:33 -0500

    feat; create auth middleware

--- internal/infrastructure/http/middleware/auth.go
+++ internal/infrastructure/http/middleware/auth.go
@@ -1,1 +1,24 @@
 package http
+
+import (
+	"strings"
+
+	pkg "github.com/alexisPerdomoD/stock-app-api/internal/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+func UserSessionMiddleware(c *gin.Context) {
+	authHeader := c.Request.Header.Get("Authorization")
+	if !strings.HasPrefix(authHeader, "Bearer ") {
+
+		c.AbortWithStatusJSON(401, gin.H{"message": "Session is required"})
+		return
+	}
+	token := strings.TrimPrefix(authHeader, "Bearer ")
+	if err := pkg.ValidateSessionToken(token); err != nil {
+		c.AbortWithStatusJSON(401, gin.H{"message": "Session is expired or invalid"})
+		return
+	}
+
+	c.Next()
+}
