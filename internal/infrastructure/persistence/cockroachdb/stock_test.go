b308891ebef7a00810c15872870a9e5242721d5b
alexisperdomod <dropdibemol@gmail.com>
2025-04-27 12:47:44 -0500

    feat: refine stock tests

--- internal/infrastructure/persistence/cockroachdb/stock_test.go
+++ internal/infrastructure/persistence/cockroachdb/stock_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/alexisPerdomoD/stock-app-api/internal/infrastructure/persistence/cockroachdb"
 	"github.com/alexisPerdomoD/stock-app-api/internal/infrastructure/service/mock"
 	"github.com/alexisPerdomoD/stock-app-api/internal/pkg"
+	"github.com/stretchr/testify/assert"
 )
 
 var stock *domain.PopulatedStock
@@ -15,7 +16,7 @@ var getAllPaginatedTest = []struct {
 	name             string
 	filter           pkg.PaginationFilter
 	userID           *uint
-	successCondition func(got *pkg.PaginationReponse[domain.PopulatedStock], t *testing.T) bool
+	successCondition func(got *pkg.PaginationReponse[domain.PopulatedStock], t *assert.Assertions) bool
 	wantErr          bool
 }{
 	{
@@ -23,48 +24,24 @@ var getAllPaginatedTest = []struct {
 		filter: pkg.PaginationFilter{},
 		successCondition: func(
 			got *pkg.PaginationReponse[domain.PopulatedStock],
-			t *testing.T,
+			a *assert.Assertions,
 		) bool {
-			t.Helper()
 
-			if got == nil {
-				t.Errorf("GetAllPaginated() got nil, want %v", got)
-				return false
-			}
+			a.NotNil(got)
+			a.Greater(len(got.Items), 0)
+			a.Condition(func() bool {
+				return len(got.Items) == 20 || len(got.Items) == got.TotalSize
+			})
 
-			if len(got.Items) == 0 {
-				t.Errorf("GetAllPaginated() got empty Items, want %v", got)
-				return false
-			}
-
-			if len(got.Items) != 20 && len(got.Items) != got.TotalSize {
-				t.Errorf("GetAllPaginated() got Items len %v, want %v",
-					len(got.Items), 20,
-				)
-				return false
-			}
-
-			if got.Page != 1 {
-				t.Errorf("GetAllPaginated() got Page %v, want %v", got.Page, 1)
-				return false
-			}
+			a.Equal(got.Page, 1)
 
 			for i, item := range got.Items {
-				if item.ID == 0 {
-					t.Errorf("GetAllPaginated() got Item %v Stock.ID %v, Invalid", i, item.ID)
-					return false
-				}
+				a.NotEqual(item.ID, 0)
 
 				if i == 0 {
 					continue
 				}
-
-				if got.Items[i-1].UpdatedAt.Before(item.UpdatedAt) {
-					t.Errorf("GetAllPaginated() got Item order DESC %v Stock.UpdatedAt %v, must be before %v",
-						i, item.UpdatedAt, got.Items[i-1].UpdatedAt)
-					return false
-				}
-
+				a.False(got.Items[i-1].UpdatedAt.Before(item.UpdatedAt))
 			}
 
 			return true
@@ -84,36 +61,23 @@ var getAllPaginatedTest = []struct {
 		},
 		successCondition: func(
 			got *pkg.PaginationReponse[domain.PopulatedStock],
-			t *testing.T,
+			t *assert.Assertions,
 		) bool {
-			t.Helper()
 
-			if got.Page != 2 {
-				t.Errorf("GetAllPaginated() got Page %v, want %v", got.Page, 2)
-				return false
-			}
+			t.Equal(got.Page, 2)
+			t.Condition(func() bool {
 
-			if got.PageSize != 5 || len(got.Items) != 5 {
-				t.Errorf("GetAllPaginated() got PageSize %v, Items len %v, want %v", got.PageSize, len(got.Items), 5)
-				return false
-			}
+				return len(got.Items) == 5 || len(got.Items) == got.TotalSize
+			})
 
 			for i, item := range got.Items {
-				if item.ID == 0 {
-					t.Errorf("GetAllPaginated() got Item %v Stock.ID %v, Invalid", i, item.ID)
-					return false
-				}
+				t.NotEqual(item.ID, 0)
 
 				if i == 0 {
 					continue
 				}
 
-				if got.Items[i-1].UpdatedAt.After(item.UpdatedAt) {
-					t.Errorf("GetAllPaginated() got Item order ASC %v Stock.UpdatedAt %v, must be after %v", i,
-						item.UpdatedAt, got.Items[i-1].UpdatedAt,
-					)
-					return false
-				}
+				t.False(got.Items[i-1].UpdatedAt.After(item.UpdatedAt))
 
 			}
 
@@ -132,6 +96,7 @@ func Test_stockRepository_Register(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to get invalid data: %v", err)
 	}
+	assert := assert.New(t)
 
 	tests := []struct {
 		name    string
@@ -153,14 +118,10 @@ func Test_stockRepository_Register(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 
 			gotErr := r.Register(context.Background(), test.data)
-			if gotErr != nil {
-				if !test.wantErr {
-					t.Errorf("Register() failed: %v", gotErr)
-				}
-				return
-			}
 			if test.wantErr {
-				t.Fatal("Register() succeeded unexpectedly")
+				assert.Error(gotErr)
+			} else {
+				assert.Nil(gotErr)
 			}
 		})
 	}
@@ -169,22 +130,16 @@ func Test_stockRepository_Register(t *testing.T) {
 func Test_stockRepository_GetAllPaginated(t *testing.T) {
 	db := cockroachdb.NewDB()
 	r := cockroachdb.NewStockRepository(db)
+	assert := assert.New(t)
 	for _, test := range getAllPaginatedTest {
 		t.Run(test.name, func(t *testing.T) {
 			got, gotErr := r.GetAllPaginated(context.Background(), test.filter, test.userID)
-			if gotErr != nil {
-				if !test.wantErr {
-					t.Errorf("GetAllPaginated() failed: %v", gotErr)
-				}
-				return
-			}
 			if test.wantErr {
-				t.Fatal("GetAllPaginated() succeeded unexpectedly")
+				assert.Error(gotErr)
+			} else {
+				assert.Nil(gotErr)
 			}
-
-			if !test.successCondition(got, t) {
-				t.Errorf("GetAllPaginated() did not pass success condition")
-			}
+			assert.True(test.successCondition(got, assert))
 		})
 	}
 }
@@ -192,7 +147,7 @@ func Test_stockRepository_GetAllPaginated(t *testing.T) {
 func Test_stockRepository_Get(t *testing.T) {
 	db := cockroachdb.NewDB()
 	r := cockroachdb.NewStockRepository(db)
-
+	assert := assert.New(t)
 	tests := []struct {
 		name    string
 		id      uint
@@ -214,63 +169,16 @@ func Test_stockRepository_Get(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, gotErr := r.Get(context.Background(), tt.id, nil)
-			if gotErr != nil {
-				if !tt.wantErr {
-					t.Errorf("Get() failed: %v", gotErr)
-				}
-				return
-			}
-
-			if tt.wantErr {
-				t.Fatal("Get() succeeded unexpectedly")
+			if !tt.wantErr {
+				assert.Nil(gotErr)
+			} else {
+				assert.Error(gotErr)
 			}
 
 			if tt.want != nil {
-				if got == nil {
-					t.Errorf("Get() got nil expected value")
-				}
-
-				if tt.want.ID != got.ID {
-					t.Errorf("Get() = ID %v, want %v", got.ID, tt.want.ID)
-
-				}
-
-				if tt.want.CompanyID != got.CompanyID {
-					t.Errorf("Get() = CompanyID %v, want %v", got.CompanyID, tt.want.CompanyID)
-				}
-
-				if *tt.want.Name != *got.Name {
-					t.Errorf("Get() = Name %v, want %v", got.Name, tt.want.Name)
-				}
-
-				if tt.want.Price != got.Price {
-					t.Errorf("Get() = Price %v, want %v", got.Price, tt.want.Price)
-				}
-
-				if tt.want.Tendency != got.Tendency {
-					t.Errorf("Get() = Tendency %v, want %v", got.Tendency, tt.want.Tendency)
-				}
-
-				if tt.want.UpdatedAt != got.UpdatedAt {
-					t.Errorf("Get() = UpdatedAt %v, want %v", got.UpdatedAt, tt.want.UpdatedAt)
-				}
-
-				if tt.want.CompanyName != got.CompanyName {
-					t.Errorf("Get() = CompanyName %v, want %v", got.CompanyName, tt.want.CompanyName)
-				}
-
-				if tt.want.Market.Name != got.Market.Name {
-					t.Errorf("Get() = Market.Name %v, want %v", got.Market.Name, tt.want.Market.Name)
-				}
-
-				if tt.want.IsSaved != got.IsSaved {
-					t.Errorf("Get() = %v, want %v", got.IsSaved, tt.want.IsSaved)
-				}
-
-			}
-
-			if tt.want == nil && got != nil {
-				t.Errorf("Get() = %v, want %v", got, tt.want)
+				assert.Equal(tt.want, got)
+			} else {
+				assert.Nil(got)
 			}
 
 		})
