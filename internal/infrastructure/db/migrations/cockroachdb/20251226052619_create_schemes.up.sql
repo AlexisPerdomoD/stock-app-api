02d33748494ef2a846ed6dc4fabc411c3033655f
perdomo <dropdibemol@gmail.com>
2025-12-28 00:26:58 -0500

    fix: refine create_schemes migration file

--- internal/infrastructure/db/migrations/cockroachdb/20251226052619_create_schemes.up.sql
+++ internal/infrastructure/db/migrations/cockroachdb/20251226052619_create_schemes.up.sql
@@ -1,4 +1,3 @@
--- Markets
 CREATE TABLE markets (
     id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name        STRING NOT NULL,
@@ -6,7 +5,6 @@ CREATE TABLE markets (
     updated_at  TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
--- Companies
 CREATE TABLE companies (
     id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     market_id   INT8 NOT NULL,
@@ -20,7 +18,6 @@ CREATE TABLE companies (
         REFERENCES markets(id)
 );
 
--- Brokerages
 CREATE TABLE brokerages (
     id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name        STRING NOT NULL,
@@ -28,7 +25,6 @@ CREATE TABLE brokerages (
     updated_at  TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
--- Users
 CREATE TABLE users (
     id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username    STRING NOT NULL UNIQUE,
@@ -40,17 +36,23 @@ CREATE TABLE users (
     updated_at  TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
--- Stocks
 CREATE TABLE stocks (
     id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     company_id  INT8 NOT NULL REFERENCES companies(id),
     ticker      STRING NOT NULL UNIQUE,
     name        STRING DEFAULT NULL,
     created_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at  TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at  TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
--- Stock Recomendation
+CREATE TABLE stock_registers(
+    id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    stock_id    INT8 NOT NULL REFERENCES stocks(id),
+    price       FLOAT8 NOT NULL,
+    tendency    INT NOT NULL,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
 CREATE TABLE stock_recommendations (
     id                  INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     brokerage_id        INT8 NOT NULL REFERENCES brokerages(id),
@@ -62,16 +64,6 @@ CREATE TABLE stock_recommendations (
     created_at          TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
--- Stocks updates
-CREATE TABLE stock_registers(
-    id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    stock_id    INT8 NOT NULL REFERENCES stock(id),
-    price       FLOAT8 NOT NULL,
-    tendency    INT NOT NULL,
-    created_at  TIMESTAMPTZ NOT NULL DEFAULT now()
-);
-
--- user stocks 
 CREATE TABLE stock_users (
     user_id INT8 NOT NULL REFERENCES users(id), 
     stock_id INT8 NOT NULL REFERENCES stocks(id),
@@ -79,10 +71,9 @@ CREATE TABLE stock_users (
     UNIQUE(user_id, stock_id)
 );
 
--- current stock tendency
-CREATE TABLE stock_tendency_stasts(
+CREATE TABLE stock_tendency_stats(
     id          INT8 PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    stock_id    INT8 NOT NULL REFERENCES stock(id) UNIQUE,
+    stock_id    INT8 NOT NULL REFERENCES stocks(id) UNIQUE,
     up_count    INT8 NOT NULL DEFAULT 0,
     side_count  INT8 NOT NULL DEFAULT 0,
     down_count  INT8 NOT NULL DEFAULT 0,
